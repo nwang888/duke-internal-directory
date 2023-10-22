96edd4fc6e8c0c482afcab192224b59fe99b2a3a
nwang888 <nwang888@gmail.com>
2023-10-22 13:43:53 -0400

    added config template, removed keys

---
+++ src/config_template.py
@@ -0,0 +1,9 @@
+# this is a template for the config.py file
+# replace the empty strings with the appropriate values
+# and rename the file to config.py
+
+import os
+
+os.environ['STREAMER_OIT_TOKEN'] = ''
+os.environ['CSV_PATH'] = ''
+os.environ['YEAR'] = ''
