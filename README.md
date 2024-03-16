a332841e1d049b709721bdbad0694d3596a56110
nwang888 <nwang888@gmail.com>
2024-03-16 12:36:01 -0400

    added fixes for new program designations

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ For example, if you wanted to validate that a netid is a valid Duke student who 
 
 Create a data folder that contains the csvs that you want to process. As of now, each csv must contain a column named "netid" that contains the netids that you want to verify. You will need to specify the path to this folder in config.py (preferably, this folder should exist in this project directory)
 
+Make sure your data has the correct format! Most importantly, there should be a column called NetID that contains the netids that you want to verify.
+
 Usage via cli:
 Navigate to the directory in which this project exists.
 Make sure to set config.py to your specified values. The parameters are dictated in the comments of the file.
@@ -22,7 +24,7 @@ The results will be stored in the data folder that you specified in config.py.
 You can also directly run the script with the parameters:
 
 ```
-python ping.py --DATA_PATH /path/to/data --YEARS 2025 Sprng,2024 Fall --PROGRAMS E-UGD,A&S
+python ping.py --DATA_PATH /path/to/data/ --YEARS 2025 Sprng,2024 Fall --PROGRAMS E-UGD,A&S
 ```
 
 Usage via Google Colab and Google Sheets:
@@ -32,8 +34,8 @@ This will hopefully be supported in the future. It is not functional right now.
 
 Current Options for Config:
 DATA_PATH: The directory of the data folder that contains the csvs that you want to process.
-YEARS: i.e. '2025 Fall', '2025 Spring' etc... Please use the format 'YYYY Term' for each eyar that you want to filter by. If you do not want to filter by year, set this to an empty list.
-PROGRAMS: i.e. 'E-UGD', 'A&S' etc... Please use the full name of the program as it appears in the directory. If you do not want to filter by program, set this to an empty list.
+YEARS: i.e. '2025 Fall','2025 Spring' etc... Please use the format 'YYYY Term' for each eyar that you want to filter by. If you do not want to filter by year, set this to an empty list. Make sure to leave no spaces between the commas.
+PROGRAMS: i.e. 'E-UGD', 'A&S' etc... Please use the full name of the program as it appears in the directory. If you do not want to filter by program, set this to an empty list. (Note. Starting with Class of 2027 they begin using A&SU for Arts and Sciences Undeclared)
 
 ## Other Notes
 
--- src/ping.py
+++ src/ping.py
@@ -22,6 +22,9 @@ def get_directory_info(netid):
     # Find the table row containing the result details
     result_detail = soup.find('div', {'class': 'person-data'})
 
+    if result_detail == None:
+        return None
+
     # Extract the affiliation
     code_string = result_detail.get_text("\n")
     
@@ -47,26 +50,30 @@ for csv_file in csv_files:
     with open(DATA_PATH + csv_file, 'r', encoding='utf-8-sig') as f:
         reader = csv.DictReader(f)
         # data = [[c.replace('\ufeff', '') for c in row] for row in reader]
-        netids = [row['netID'] for row in reader]
+        netids = [row['NetID'] for row in reader]
         # fieldnames = [fieldname.replace('\ufeff', '') for fieldname in reader.fieldnames]
 
     # write to a new csv file that contains netid and valid
     with open(DATA_PATH + csv_file[:-4] + 'OUT.csv', 'w', encoding='utf-8-sig') as f:
-        writer = csv.DictWriter(f, fieldnames=['netID', 'valid'], lineterminator="\n")
+        writer = csv.DictWriter(f, fieldnames=['netID', 'valid', 'noInfo', 'noGradTerm', 'invalidYear', 'noProgram', 'invalidProgram'], lineterminator="\n")
         writer.writeheader()
 
         # go through each netid and check if it exists and graduation term matches the year
         for netid in netids:
             isValid = True
             directory_details = get_directory_info(netid)
-            if (directory_details == None or
-                'Graduation Term' not in directory_details.keys() or
-                directory_details['Graduation Term'] not in YEARS or
-                'Program' not in directory_details.keys() or
-                directory_details['Program'] not in PROGRAMS):
+            noInfo = directory_details == None
+            if noInfo:
                 isValid = False
+            else: 
+                noGradTerm = 'Graduation Term' not in directory_details.keys()
+                invalidYear = directory_details['Graduation Term'] not in YEARS
+                noProgram = 'Program' not in directory_details.keys()
+                invalidProgram = directory_details['Program'] not in PROGRAMS
+                if (noInfo or noGradTerm or invalidYear or noProgram or invalidProgram):
+                    isValid = False
             if isValid:
                 writer.writerow({'netID': netid, 'valid': 'True'})
             else:
-                writer.writerow({'netID': netid, 'valid': 'False'})
+                writer.writerow({'netID': netid, 'valid': 'False', 'noInfo': noInfo, 'noGradTerm': noGradTerm, 'invalidYear': invalidYear, 'noProgram': noProgram, 'invalidProgram': invalidProgram})
         print('done')
